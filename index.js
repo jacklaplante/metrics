6ce810161ecb0fd27295e72ac82d3e58818389f3
Jack LaPlante <laplante.jack@gmail.com>
2020-01-15 07:50:21 -0500

    working on getting locations from IP address

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ function fetchData() {
       let body = document.getElementById("metrics-table").getElementsByTagName('tbody')[0]
       json.Items.forEach(item => {
         let row = body.insertRow()
-        if (item.timestamp)     row.insertCell(0).innerText = item.timestamp.N
-        if (item.serverId)      row.insertCell(1).innerText = item.serverId.S
+        if (item.timestamp) row.insertCell(0).innerText = item.timestamp.N
+        if (item.serverId) row.insertCell(1).innerText = item.serverId.S
         if (item.activePlayers) row.insertCell(2).innerText = item.activePlayers.S
-        if (item.status)        row.insertCell(3).innerText = item.status.S
-        if (item.playerIp)      row.insertCell(4).innerText = item.playerIp.S
+        if (item.status) row.insertCell(3).innerText = item.status.S
+        if (item.playerIp) {
+          let playerIp = item.playerIp.S
+          row.insertCell(4).innerText = playerIp
+          // fetchLocation(playerIp, row)
+        }
       })
     })
   })
+}
+
+
+let locations = {}
+function fetchLocation(ip, row) {
+  ip = ip.replace("::ffff:", "")
+  if (ip.split('.').length >= 4 && !locations[ip]) {
+    fetch('https://api.ipgeolocationapi.com/geolocate/' + ip, {mode: 'no-cors'}).then(response => {
+      response.json().then(json => {
+        // add location to map
+        row.insertCell(5).innerText = "location"
+      })
+    }) 
+  }
 }
