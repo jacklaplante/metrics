53ba7e2bba99d86b1e844d8c9bb216b52c809b72
Jack LaPlante <laplante.jack@gmail.com>
2020-01-17 12:13:06 -0500

    format timestamp and IP address

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ function fetchData() {
       let body = document.getElementById("metrics-table").getElementsByTagName('tbody')[0]
       json.Items.forEach(item => {
         let row = body.insertRow()
-        if (item.timestamp) row.insertCell(0).innerText = item.timestamp.N
+        if (item.timestamp) {
+          let date = new Date(parseInt(item.timestamp.N))
+          row.insertCell(0).innerText = date.toString()
+        }
         if (item.serverId) row.insertCell(1).innerText = item.serverId.S
         if (item.activePlayers) row.insertCell(2).innerText = item.activePlayers.S
         if (item.status) row.insertCell(3).innerText = item.status.S
         if (item.playerIp) {
-          let playerIp = item.playerIp.S
+          let playerIp = item.playerIp.S.replace("::ffff:", "")
           row.insertCell(4).innerText = playerIp
           // fetchLocation(playerIp, row)
         }
@@ -21,7 +24,6 @@ function fetchData() {
 
 let locations = {}
 function fetchLocation(ip, row) {
-  ip = ip.replace("::ffff:", "")
   if (ip.split('.').length >= 4 && !locations[ip]) {
     fetch('https://api.ipgeolocationapi.com/geolocate/' + ip, {mode: 'no-cors'}).then(response => {
       response.json().then(json => {
